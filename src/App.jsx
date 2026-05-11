f36c9fa11741adfdb7c1063ffef115f8a82ff0d3
samsul <csamsul2017@gmail.com>
2026-05-11 21:11:02 +0700

    feat: adding section about me

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,27 @@
 import React from 'react';
-import { FaUserLarge, FaRegUser, FaChevronDown, FaInstagram, FaTwitter, FaLinkedin, FaGithub } from 'react-icons/fa6';
+import { FaUserLarge, FaEnvelope, FaLocationDot, FaBriefcase, FaRegUser, FaChevronDown, FaInstagram, FaTwitter, FaLinkedin, FaGithub, FaUser } from 'react-icons/fa6';
 
 const App = () => {
   return (
     <div>
-      <section className="py-4 bg-black text-white lg:flex lg:flex-col lg:justify-center lg:items-center">
+      <section className="py-4 bg-[#02040F] text-white lg:flex lg:flex-col lg:justify-center lg:items-center">
         <header className="h-14 border-b px-6 lg:w-full">
           <h2 className="text-2xl font-semibold">MyProfile.</h2>
         </header>
 
         <div className="lg:w-1/2">
           <div className="flex flex-col items-center px-6 md:flex-row md:gap-12 md:pt-24 md:justify-center">
-            <div className="shrink-0 border bg-amber-50 h-72 w-72 rounded-full overflow-hidden my-10">
+            <div className="shrink-0  ring-4  ring-[#6756FD]/50 bg-amber-50 h-72 w-72 rounded-full overflow-hidden my-10">
               <img src="./hero.webp" className="h-full w-full object-cover object-[center_24px]" alt="Muhamad Samsul - Developer" />
             </div>
 
             <div className="flex flex-col gap-4">
-              <span className="text-xl">Hi, I'm</span>
+              <span className="text-xl text-[#7e6fff]">Hi, I'm</span>
               <h1 className="text-6xl">Samsul</h1>
-              <span className="text-2xl">Web Developer</span>
+              <span className="text-2xl text-[#7e6fff]">Web Developer</span>
               <p className="text-lg tracking-wider">I build, responsive, and user-friendly websites that bring ideas to life.</p>
               <div className="flex flex-col gap-4 font-semibold md:flex-row whitespace-nowrap">
-                <button className="border rounded-2xl py-4 flex justify-center items-center gap-2 md:px-8">
+                <button className="bg-[#7e6fff] rounded-2xl py-4 flex justify-center items-center gap-2 md:px-8">
                   <FaRegUser />
                   <span>About Me</span>
                 </button>
@@ -38,6 +38,29 @@ const App = () => {
             <FaLinkedin />
             <FaTwitter />
             <FaInstagram />
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div>
+          <h2>About Me</h2>
+          <p>I'm a passionate web developer who enjoys creating beautoful and functional websites. I love learning nw technologies and turning complex problems into simple, elegent solutions.</p>
+          <div>
+            <div>
+              <div>
+                <FaUser /> Name
+              </div>
+              <div>
+                <FaEnvelope /> Email
+              </div>
+              <div>
+                <FaLocationDot /> Location
+              </div>
+              <div>
+                <FaBriefcase /> Freelance
+              </div>
+            </div>
           </div>
         </div>
       </section>
