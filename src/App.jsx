6281d221d277d526d527a4ea5281c21f56b61cca
samsul <csamsul2017@gmail.com>
2026-05-14 14:11:50 +0700

    feat: adding skill sextion in mobile menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
 import { FaUserLarge, FaEnvelope, FaLocationDot, FaBriefcase, FaRegUser, FaChevronDown, FaInstagram, FaTwitter, FaLinkedin, FaGithub, FaUser } from 'react-icons/fa6';
+import { LuUser, LuMail, LuMapPin, LuBriefcaseBusiness } from 'react-icons/lu';
+import { html5, cssIcon, jsIcon, reactIcon, tailwindIcon, githubIcon } from './assets';
+
+const identities = [
+  { Icon: <LuUser />, title: 'Name', value: 'Samsul', borderB: true },
+  { Icon: <LuMail />, title: 'Email', value: 'team@samsul.dev', borderB: true },
+  { Icon: <LuMapPin />, title: 'Location', value: 'Indonesia', borderB: true },
+  { Icon: <LuBriefcaseBusiness />, title: 'Freelance', value: 'Available', color: '#7E6FFF', borderB: false },
+];
+
+const skillList = [
+  { img: html5, title: 'HTML', value: 95 },
+  { img: cssIcon, title: 'CSS', value: 90 },
+  { img: jsIcon, title: 'JavaScript', value: 80 },
+  { img: reactIcon, title: 'React', value: 80 },
+  { img: tailwindIcon, title: 'Tailwind CSS', value: 80 },
+  { img: githubIcon, title: 'Git & Gituhub', value: 80 },
+];
 
 const App = () => {
+  const color = true;
+  const borderB = false;
+
   return (
     <div>
       <section className="py-4 bg-[#02040F] text-white lg:flex lg:flex-col lg:justify-center lg:items-center">
@@ -42,26 +63,51 @@ const App = () => {
         </div>
       </section>
 
-      <section>
-        <div>
-          <h2>About Me</h2>
+      <section className="py-12 bg-[#F7F8FB]">
+        <div className="px-8 flex flex-col gap-4">
+          <h2 className="flex gap-2 items-center text-2xl font-semibold">
+            <FaUser color="#7E6FFF" /> About Me
+          </h2>
           <p>I'm a passionate web developer who enjoys creating beautoful and functional websites. I love learning nw technologies and turning complex problems into simple, elegent solutions.</p>
+
           <div>
-            <div>
-              <div>
-                <FaUser /> Name
-              </div>
-              <div>
-                <FaEnvelope /> Email
-              </div>
-              <div>
-                <FaLocationDot /> Location
-              </div>
-              <div>
-                <FaBriefcase /> Freelance
-              </div>
+            <div className="flex flex-col bg-white rounded-2xl px-8 shadow-gray-600 text-lg font-medium">
+              {identities.map(identity => (
+                <div className={`flex gap-14 ${identity.borderB && 'border-b'}  border-b-gray-200 py-4`}>
+                  <div className="flex flex-1 items-center gap-2">
+                    {identity.Icon} {identity.title}
+                  </div>
+                  <div className="flex-2">
+                    <span className={`${identity.color && 'text-[#7E6FFF]'}`}>{identity.value}</span>
+                  </div>
+                </div>
+              ))}
             </div>
           </div>
+        </div>
+      </section>
+
+      <section className="py-12 bg-[#F7F8FB] px-8">
+        <div className="flex flex-col gap-8">
+          <h2 className="text-2xl font-semibold">
+            <span className="text-[#7E6FFF] pr-2">{'</>'}</span> Skills
+          </h2>
+          {skillList.map(skill => (
+            <div className="flex gap-4 items-center">
+              <div className="h-10 w-10">
+                <img src={skill.img} alt="" />
+              </div>
+              <div className="flex-1 flex flex-col gap-2">
+                <div className="flex justify-between font-semibold">
+                  <span>{skill.title}</span>
+                  <span>{`${skill.value} %`}</span>
+                </div>
+                <div className="h-2 rounded-4xl bg-gray-200 ">
+                  <div style={{ width: `${skill.value}%` }} className={`h-full bg-[#7E6FFF] rounded-4xl ring-1 ring-gray-200`}></div>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
       </section>
     </div>
---
+++ src/assets/index.js
@@ -0,0 +1,8 @@
+import html5 from './html-5.png';
+import cssIcon from './css-3.png';
+import jsIcon from './js.png';
+import reactIcon from './physics.png';
+import tailwindIcon from './tailwindcss.png';
+import githubIcon from './github.png';
+
+export { html5, cssIcon, jsIcon, reactIcon, tailwindIcon, githubIcon };
